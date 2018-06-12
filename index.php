0e6a332c7a65c7da408f9beb2f793020d0337b16
Claudio <claudio.schwarz@gmail.com>
2018-06-12 22:36:57 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,52 +1,15 @@
 <?php get_header(); ?>
-    <div class="container grid-lines">
-        <div class="row">
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-            <div class="col-sm-1">
-                <div class="grid-lines__col"></div>
-            </div>
-        </div>
-    </div>
     <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : ?>
-            <?php the_post(); ?>
+        <section class="posts">
             <div class="container">
-                <div class="row equal-height">
-                    <div class="col-sm-5">
-                        <div class="card">
-                            <?php get_template_part('view/index/content'); ?>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card submit-box">
-                            <p>
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                            </p>
-                        </div>
-                    </div>
+                <div class="row">
+	                <?php while (have_posts()) : ?>
+		                <?php the_post(); ?>
+                        <?php get_template_part('view/index/content'); ?>
+	                <?php endwhile; ?>
                 </div>
             </div>
-        <?php endwhile; ?>
+        </section>
     <?php else : ?>
         <?php get_template_part('view/global/content-404'); ?>
     <?php endif; ?>
--- view/index/content.php
+++ view/index/content.php
@@ -1,2 +1,4 @@
-<?php the_post_thumbnail(); ?>
-<?php the_content(); ?>
+<article <?php post_class( 'col-lg-4' ); ?>>
+	<?php the_post_thumbnail(); ?>
+	<?php the_content(); ?>
+</article>
